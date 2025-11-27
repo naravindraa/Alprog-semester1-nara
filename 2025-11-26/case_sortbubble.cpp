863af7cf4b1971b03336e29070c33422aa8b1bf9
naravindraa <rascanara01@gmail.com>
2025-11-27 17:28:34 +0700

    case_sortbubble.cpp

---
+++ 2025-11-26/case_sortbubble.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Masukkan jumlah nilai: ";
+    cin >> n;
+
+    int nilai[n];
+    cout << "Masukkan nilainya: ";
+    for (int i = 0; i < n; i++)
+        cin >> nilai[i];
+
+    bubbleSort(nilai, n);
+
+    cout << "\nNilai setelah diurutkan: ";
+    for (int i = 0; i < n; i++)
+        cout << nilai[i] << " ";
+
+    return 0;
+}
